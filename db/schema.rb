4803137f7e02e52948e59349f09cfb7724bb6d4a
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-11-28 14:14:49 +0000

    [Add]1-3

--- db/schema.rb
+++ db/schema.rb
@@ -40,15 +40,6 @@ ActiveRecord::Schema.define(version: 2024_11_28_124054) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "encrypted_password"
-    t.boolean "is_active"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
