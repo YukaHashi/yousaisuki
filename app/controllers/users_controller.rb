43f5d729a37fead15cc55d4efa38a58ac3b06906
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-11-30 13:31:21 +0000

    [Add]1-15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
     @user = User.find(params[:id])
     @post = @user.posts
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
