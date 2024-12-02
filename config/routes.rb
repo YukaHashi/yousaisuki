3977796c65c76625650f98d2bc5b2c211247a8fb
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-12-02 11:04:51 +0000

    [Add]1-16

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get '/homes/about' => "homes#about"
+  get '/users/mypage' => "users#mypage"
   
   resources :posts, only: [:new, :create, :index, :show, :edit, :destroy]
   resources :users, only: [:show, :edit, :update]
   
-  get '/users/mypage' => "users#mypage"
 end
