79a2d56dfd2d0fc46dffbac021dd610fae77d623
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-12-04 13:52:33 +0000

    [Add]フェーズ1,実装4-4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,23 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path
+    redirect_to user_path(@user.id)
+  end
+  
+  def withdraw
+    @user = User.find(current_user.id)
+    # is_deletedカラムをtrueにupdateにして退会状態にする
+    @user.update(is_deleted: true)
+    # セッション情報を全て削除
+    reset_session
+    flash[:notice] = "退会処理を実行しました"
+    # 退会後トップ画面に遷移する
+    redirect_to root_path
+  end
+  
+  # is_deletedがfalseならtrueを返すように
+  def active_for_authentication?
+    super && (is_deleted == false)
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get '/homes/about' => "homes#about"
   get '/users/mypage' => "users#mypage"
+  # 退会確認画面
+  get '/users/check' => "users#check"
+  # 退会処理（論理削除）
+  patch '/users/withdraw' => "users#withdraw"
   
   resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update]
