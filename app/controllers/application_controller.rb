5e44741a78d7c0346505b98bec55326237a50115
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-12-16 07:06:44 +0000

    [Add]1st本番環境でのテスト1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にメソッド実行
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  # サインイン後の遷移先を設定
   def after_sign_in_path_for(resource)
-    posts_path
+    users_mypage_path
   end
   
+  # サインアウト後の遷移先を設定
   def after_sign_out_path_for(resource)
     homes_about_path
   end
   
   protected
   
+  # ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
