443c523b0a4bfcad8b86ec55959811594ff3157b
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-12-26 07:57:06 +0000

    Adminでログイン、ログアウト後の遷移先を設定

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,4 +16,15 @@ class SessionsController < ApplicationController
       flash[:notice] = "該当するユーザーが見つかりません"
     end
   end
+  
+  # ログイン後の遷移先の設定
+  def after_sign_in_path_for(resource)
+    admin_dashboards_path
+  end
+  
+  # ログアウト後の遷移先の設定
+  def after_sign_out_path_for(resource_or_scope)
+    new_admin_session_path
+  end
+  
 end
