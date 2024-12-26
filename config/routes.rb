213b1f2824484ede21ffc818bdd31335ac02304f
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-12-26 08:07:33 +0000

    Adminのユーザー一覧表示を作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     sessions: 'admin/sessions'
   }
   
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+  end
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
