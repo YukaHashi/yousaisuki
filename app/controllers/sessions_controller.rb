70569b863b19b1df552de229788c5d68d10d4e49
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-12-26 08:24:55 +0000

    Adminのログアウト機能を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,14 +17,4 @@ class SessionsController < ApplicationController
     end
   end
   
-  # ログイン後の遷移先の設定
-  def after_sign_in_path_for(resource)
-    admin_dashboards_path
-  end
-  
-  # ログアウト後の遷移先の設定
-  def after_sign_out_path_for(resource_or_scope)
-    new_admin_session_path
-  end
-  
 end
