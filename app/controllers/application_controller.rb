2e2a2fed1898ec4b0efe68aab57af8a7200e417f
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-12-26 08:41:52 +0000

    Adminの権限の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  # ログイン認証が済んでいない状態でトップページ以外にアクセスしても、ログイン画面へリダイレクトする
-  before_action :authenticate_user!, except: [:top]
+  # ログイン認証が済んでいない状態でトップページ以外にアクセスしても、ログイン画面へリダイレクトする(管理者以外)
+  before_action :authenticate_user!, except: [:top], unless: :admin_controller?
   # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にメソッド実行
   before_action :configure_permitted_parameters, if: :devise_controller?
   
@@ -12,6 +12,12 @@ class ApplicationController < ActionController::Base
   # サインアウト後の遷移先を設定
   def after_sign_out_path_for(resource)
     root_path
+  end
+  
+  private
+  
+  def admin_controller?
+    safe.class.module_parent_name == 'Admin'
   end
   
   protected
