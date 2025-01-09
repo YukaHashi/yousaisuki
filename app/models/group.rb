7e9526dd141238b4094c12126a04840b66b601c0
YukaHashi <yuka.hashimotorenge@gmail.com>
2025-01-09 12:55:24 +0000

    2ndデプロイ完了

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,7 +5,7 @@ class Group < ApplicationRecord
   
   validates :name, presence: true
   validates :introduction, presence: true
-  has_one_attached :image
+  has_one_attached :group_image
   
   def is_owned_by?(user)
     owner.id == user.id
