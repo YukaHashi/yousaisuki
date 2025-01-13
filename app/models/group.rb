66ab9274c41c7c50e8ec5e9159d5abbb340ceba7
YukaHashi <yuka.hashimotorenge@gmail.com>
2025-01-13 13:37:03 +0000

    groupのエラーの修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,5 +10,13 @@ class Group < ApplicationRecord
   def is_owned_by?(user)
     owner.id == user.id
   end
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image
+  end
 
 end
