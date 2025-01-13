a6a5fa10b08a028cb0cbef05ff89b1f470b07370
YukaHashi <yuka.hashimotorenge@gmail.com>
2025-01-13 13:44:54 +0000

    groupのエラーの修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -12,11 +12,11 @@ class Group < ApplicationRecord
   end
   
   def get_image
-    unless image.attached?
+    unless group_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-      image
+      group_image
   end
 
 end
