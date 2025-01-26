2b9e228396a0cfc64f81b1853fce12b741696e19
YukaHashi <yuka.hashimotorenge@gmail.com>
2025-01-26 13:54:30 +0000

    グループ画面の修正

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -25,7 +25,7 @@ class Public::GroupsController < ApplicationController
     if @group.save
       redirect_to groups_path
     else
-      render 'new'
+      render :new
     end
   end
   
@@ -34,17 +34,20 @@ class Public::GroupsController < ApplicationController
   end
   
   def update
+    @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to groups_path
+      flash[:notice] = "保存に成功しました"
+      redirect_to groups_path(@group.id)
     else
-      render 'edit'
+      flash.now[:notice] = "保存に失敗しました"
+      render :edit
     end
   end
   
   private
   
   def group_params
-    params.require(:group).permit(:name, :introduction, :image)
+    params.require(:group).permit(:name, :introduction, :group_image)
   end
   
   # params[:id]を持つ@groupのowner_idカラムのデータと自分のuser_idが一緒かを確かめる
